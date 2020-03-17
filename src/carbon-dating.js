506111c2a5fd52731fef57f1c56eceeb30c893e0
iryna.blotskaya <charlie.the.unicorn.blbl@gmail.com>
2020-03-17 15:22:49 +0300

    feat: add solution to cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let result = true
+
+  if(typeof sampleActivity !== 'string' || isNaN(parseFloat(sampleActivity)) || (Number(sampleActivity) <= 0 || Number(sampleActivity) > 15 ) ){
+    return false
+  }
+
+  result = Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity))/(0.693/HALF_LIFE_PERIOD))
+  
+
+
+
+  return result
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  
+  let x = 0
+  for(let i =0; i <= matrix.length - 1; i++ ){
+    matrix[i].forEach(el => {
+      if(el === '^^'){
+        x += 1
+      }
+    });
+  }
+  return x
 };
