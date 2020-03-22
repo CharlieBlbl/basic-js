20371e0bdc52bd5beeb1f0a2e9d82178d64ad9ab
iryna.blotskaya <charlie.the.unicorn.blbl@gmail.com>
2020-03-22 18:13:23 +0300

    feat: add solution to transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,38 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+ let res = []
+    if (!(arr instanceof Array)){
+         throw 'Not implemented';
+    } 
+    if (arr.length === 0) return arr;
+    
+    for(let i = 0; i <= arr.length - 1; i++){
+
+        if(arr[i] === "--double-next") {
+            if(i < arr.length - 1) {
+                res.push(arr[i+1])
+            }           
+        } else if(arr[i] === "--double-prev") {
+            if(i > 0) {
+                res.push(arr[i-1])
+            }
+        }else if (arr[i] === "--discard-prev") {
+                if(i > 0) {
+                    res.pop()                    
+                }
+        }else if (arr[i] === "--discard-next") {
+                if(i < arr.length - 1) {
+                    i++}                    
+        }else{
+                res.push(arr[i])
+                } 
+            
+
+            
+        
+    }
+  
+    return res
 };
+
+
+
