a6e55c0f16e2216d7563629fd20624a15353ea2b
iryna.blotskaya <charlie.the.unicorn.blbl@gmail.com>
2020-03-22 23:06:23 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,16 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let   repeatTimes = options.repeatTimes || 1
+    let   separator = options.separator || '+'
+    let   addition = options.addition !== undefined ?  options.addition : ''
+    let   additionRepeatTimes = options.additionRepeatTimes || 1
+    let   additionSeparator = options.additionSeparator || '|'
+    
+    const additionString = (new Array(additionRepeatTimes))
+        .fill(`${addition}`)
+        .join(additionSeparator)
+
+    return resultString = (new Array(repeatTimes))
+        .fill(`${str}${additionString}`)
+        .join(separator)    
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,57 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(value){
+        this.value = value
+    }
+    encrypt(message, key) {
+        if (!message || !key){ throw new Error}
+        message = message.toUpperCase()
+        key = key.toUpperCase()
+        
+        let res = ''
+        let alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let n = alpha.length
+
+        for (let i = 0, j = 0; i < message.length; i++, j++){
+            if(alpha.indexOf(message[i] !== -1)){
+                let m = alpha.indexOf(message[i])
+                let k = alpha.indexOf(key.charAt(j % key.length))
+                res +=  alpha [(m + k) % n]
+            } else {
+                j =j-1
+                res += message[i]
+            }
+        }
+        if (this.value === false) {return res.split('').reverse().join('')} 
+        else {
+            return res
+        }
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key){ throw new Error}
+        message = message.toUpperCase()
+        key = key.toUpperCase()
+       
+        let res = ''
+        let alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let n = alpha.length
+
+        for (let i = 0, j = 0; i < message.length; i++, j++) {
+            if (alpha.indexOf(message[i] !== -1)) {
+                let c = alpha.indexOf(message[i])
+                let k = alpha.indexOf(key.charAt(j % key.length))                
+                res += alpha[(c + n - k) % n];
+            } else {
+                k = k - 1;
+                result += message[i];
+            }
+        }
+
+
+        if (this.value === false) {return res.split('').reverse().join('')} 
+        else {
+            return res
+        }
     }
 }
 
